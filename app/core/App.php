4fee8f5aff8cf0cdeb498c5cd7d892d7293f2269
Ardhian Syachrial <syachrialardhian50@gmail.com>
2019-10-06 21:16:38 +0700

    MVC Routing

--- app/core/App.php
+++ app/core/App.php
@@ -2,6 +2,15 @@
 
 class App{
 	public function __construct(){
-		echo "OK";
+		$url = $this->parseURL();
+	}
+
+	public function parseURL(){
+		if(isset($_GET['url'])){
+			$url = rtrim($_GET['url'], '/');
+			$url = filter_var($url, FILTER_SANITIZE_URL);
+			$url = explode('/', $url);
+			return $url;
+		}
 	}
 }
