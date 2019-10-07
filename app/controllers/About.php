bc471203a6425bdd86da7caa1b848cf8924847ce
Ardhian Syachrial <syachrialardhian50@gmail.com>
2019-10-07 17:14:00 +0700

    Menambahkan MVC Views

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,11 +1,20 @@
 <?php 
 
-class About {
+class About extends Controller{
 	public function index($nama = 'Ardhian', $pekerjaan = 'Mahasiswa'){
-		echo "Halo, nama saya $nama, saya adalah seorang $pekerjaan";
+		$data['nama'] = $nama;
+		$data['pekerjaan'] = $pekerjaan;
+		$data['judul'] = 'About Me';
+		$this->view('templates/header', $data);
+		$this->view('about/index', $data);
+		$this->view('templates/footer');
 	}
+
 	public function page(){
-		echo "About/page";
+		$data['judul'] = 'Pages';
+		$this->view('templates/header', $data);
+		$this->view('about/page');
+		$this->view('templates/footer');
 	}
 }
- ?>
+ ?> 
---
+++ app/views/about/index.php
@@ -0,0 +1,3 @@
+
+	<h1>About Me</h1>
+	<p>Halo, Nama Saya <?= $data['nama'] ?>, Saya adalah seorang <?= $data['pekerjaan'] ?></p>
