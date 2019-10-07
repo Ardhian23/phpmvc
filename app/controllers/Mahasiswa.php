e3f23fd194f1caa618cc8b35a17220a6c1a0308e
Ardhian Syachrial <syachrialardhian50@gmail.com>
2019-10-07 21:21:41 +0700

    Menambahkan database wrapper

--- app/controllers/Mahasiswa.php
+++ app/controllers/Mahasiswa.php
@@ -3,8 +3,16 @@ class Mahasiswa extends Controller{
 	public function index(){
 		$data['judul'] = 'Daftar Mahasiswa';
 		$data['mhs'] = $this->model('mahasiswa_model')->getAllMahasiswa();
-		$this->view('templates/header');
+		$this->view('templates/header', $data);
 		$this->view('mahasiswa/index', $data);
+		$this->view('templates/footer');
+	}
+
+		public function detail($id){
+		$data['judul'] = 'Detail Mahasiswa';
+		$data['mhs'] = $this->model('mahasiswa_model')->getMahasiswaById($id);
+		$this->view('templates/header', $data);
+		$this->view('mahasiswa/detail', $data);
 		$this->view('templates/footer');
 	}
 }
