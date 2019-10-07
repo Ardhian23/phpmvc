2d3666e902349ce8ab4f3b2385ad1f47c3cfc91f
Ardhian Syachrial <syachrialardhian50@gmail.com>
2019-10-07 18:30:29 +0700

    Menambahkan assets dan menambahkan bootstrap

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,5 @@
-
-	<h1>About Me</h1>
+<div class="container">
+	<h1 class="mt-4">About Me</h1>
+	<img src="<?= BASEURL; ?>/img/mikrotik.jpg" width = "200" class = "rounded-circle shadow">
 	<p>Halo, Nama Saya <?= $data['nama'] ?>, Saya adalah seorang <?= $data['pekerjaan'] ?></p>
+</div>
